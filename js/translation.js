ef4bb01665ebdc77c569581f3f1fe3f1086e97db
leito9008 <leito.vm3@hotmail.com>
2015-03-24 23:41:58 -0300

    - Basic i18n configuration
    - Basic translation files (en, es)
    - Some images changed + fav icon
    - CSS header modifications

---
+++ js/translation.js
@@ -0,0 +1,8 @@
+/* jshint eqeqeq: true, quotmark: single, undef: true, unused: true, strict: true, jquery: true */
+/* global i18n */
+
+i18n.init(function () {
+    'use strict';
+
+    $('html').i18n();
+});
